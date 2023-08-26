565471e98a1da577b52311f8379f7aef99d5c8e8
Deadline Was Yesterday <deadlinewasyesterday@gmail.com>
2023-08-25 20:31:20 -0700

    betafix

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #new env w/ dependencies
-conda create -y -n "phyloBUSCO-beta" -c anaconda -c conda-forge -c bioconda -c r r catfasta2phyml iqtree biopython compleasm blast python=3.7.12 
+conda create -y -n "phyloBUSCO-beta" -c anaconda -c conda-forge -c bioconda -c r r parallel catfasta2phyml iqtree biopython compleasm blast python=3.7.12 
